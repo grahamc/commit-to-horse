ba52c837ce57114fa90f81041fd9a7ecce184bcb
Graham Christensen <graham@grahamc.com>
2014-01-08 16:17:01 -0600

    Make committers the horse they are.

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,42 +1,67 @@
-walk(document.body);
 
-function walk(node) 
+// Hackz... I'm sorry.
+String.prototype.repeat = function( num )
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    return new Array( Math.ceil(num + 1) ).join( this );
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+var commitToHorse = new function() {
+  this.whin = true;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+  this.classpath = function(elem_type, class_name) {
+    return "//" + elem_type + "[contains(concat(' ',normalize-space(@class),' '),' " + class_name + " ')]"
+  }
+
+  this.go = function() {
+    var paths = [
+      this.classpath('p', 'commit-title') + '/a',
+      this.classpath('div', 'commit-desc') + '/pre'
+    ]
+
+    var targets = []
+
+    var elems;
+    for (var i = 0; i < paths.length; i++) {
+      elems = document.evaluate(paths[i], document, null, XPathResult.ANY_TYPE, null );
+      var commit;
+
+      while (commit = elems.iterateNext()) {
+        targets.push(commit);
+      }
+    }
+
+    for (var i = 0; i < targets.length; i++) {
+      targets[i].textContent = this.rehorse(targets[i].textContent);
+    }
+  }
+
+  this.rehorse = function(txt)
+  {
+    var v = "";
+
+    var words = txt.split(/([\w']+)/)
+    for (var i = 0; i < words.length; i++) {
+      wordlen = words[i].length;
+      if (wordlen < 4) {
+        v += words[i];
+      } else if (wordlen == 4) {
+        v += "neigh";
+      } else {
+        halflen = wordlen / 2;
+        if (this.whin) {
+          this.whin = !this.whin;
+          v += "wh" + "i".repeat(halflen) + "n" + "e".repeat(halflen) + "y";
+        } else {
+          v += "n" + "e".repeat(halflen) + "i".repeat(halflen) + "gh"
+        }
+      }
+    }
+
+    this.whin = !this.whin;
+
+    return v;
+  }
 }
 
+commitToHorse.go()
 
